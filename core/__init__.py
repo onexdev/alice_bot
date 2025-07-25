c52b5ac455fca818d954eb3d0cc73e234807b659
onexdev <nyvisme@gmail.com>
2025-07-25 11:18:50 +0700

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,19 +1,19 @@
-“””
+"""
 ALICE Bot Core Package
 Advanced BSC Scanner Core Components
-“””
+"""
 
-**version** = “1.0.0”
-**author** = “onex_dv”
+__version__ = "1.0.0"
+__author__ = "onex_dv"
 
 from .scanner import BSCScanner
 from .config import Config
 from .utils import DataFormatter, AddressValidator, DataFilter
 
-**all** = [
-‘BSCScanner’,
-‘Config’,
-‘DataFormatter’,
-‘AddressValidator’,
-‘DataFilter’
+__all__ = [
+    'BSCScanner',
+    'Config', 
+    'DataFormatter',
+    'AddressValidator',
+    'DataFilter'
 ]
